70f58a2408f0c4a10a6b8ef3448d490350caeecc
aanderton-OFGEM <ashley.anderton@ofgem.gov.uk>
2025-05-28 11:12:41 +0100

    Many many fixes for tech flow and others

--- app/routes/v31.js
+++ app/routes/v31.js
@@ -6023,7 +6023,15 @@ router.post('/' + version + '/add-heat-network/energycentre/addresscoords', func
         });
     }
     else {
-            res.redirect('/' + version + '/add-heat-network/energycentre/addressconfirm');
+            var energycentres = req.session.data['energycentres'] 
+
+    if (energycentres == 0) {
+        res.redirect('/' + version + '/add-heat-network/energycentre/cya');
+
+    }
+    else {
+        res.redirect('/' + version + '/add-heat-network/energycentre/type');
+    }
     }
 });
 
@@ -6083,38 +6091,76 @@ router.post('/' + version + '/add-heat-network/energycentre/address', function (
 
         const apiKey = 'HDNGKBm2TGbHTt2mr4RxS2Ta0l2Gwth6';
 
+        const customSort = (a, b) => {
+            const extractNumber = (str) => {
+              const match = str.match(/^\d+/); // Extracts the leading number from the string
+              return match ? parseInt(match[0], 10) : NaN; // Converts the extracted number to integer
+            };
+          
+            const numA = extractNumber(a);
+            const numB = extractNumber(b);
+          
+            if (!isNaN(numA) && isNaN(numB)) {
+              return -1;
+            } else if (isNaN(numA) && !isNaN(numB)) {
+              return 1;
+            } else if (!isNaN(numA) && !isNaN(numB)) {
+              return numA - numB; // Compare the numbers if both are numbers
+            } else {
+              return a.localeCompare(b); // Compare as strings if neither is a number
+            }
+          };
         async function postcode(postcode) {
             axios.get('https://api.os.uk/search/places/v1/postcode?postcode=' + postcode + '&dataset=LPI&key=' + apiKey, { httpsAgent })
                 .then(function (response) {
                     var output = JSON.stringify(response.data, null, 2);
+                    let totalResults = response.data.header.totalresults;
                     let parsed = JSON.parse(output).results;
                     let locationaddresses = [];
                     if (parsed != undefined) {
-
                         for (var i = 0; i < parsed.length; i++) {
                             let obj = parsed[i];
                             locationaddresses.push(obj.LPI.ADDRESS);
                         }
-                        req.session.data.ecAddressSelect = locationaddresses;
+        
+                        req.session.data.ecAddressSelect = locationaddresses.sort(customSort);
                         req.session.data.ecorgaddressesnotfound = "";
-                        res.redirect('/' + version + '/add-heat-network/energycentre/addressselect');
+                        if (totalResults > 99) {
+                            res.redirect('/' + version + '/add-heat-network/energycentre/addresserror?reason=toomany');
+                        }
+                        else {
+                            res.redirect('/' + version + '/add-heat-network/energycentre/addressselect');
+                        }
                     }
-
+        
                     else {
                         req.session.data.ecAddressSelect = locationaddresses;
-                        req.session.data.ecaddressesnotfound = true;
-                        res.redirect('/' + version + '/add-heat-network/energycentre/addressmanual');
+                        req.session.data.orgaddressnotfound = true;
+                        res.redirect('/' + version + '/add-heat-network/energycentre/addresserror');
+        
                     }
-
+        
                 });
-
+        
         }
         postcode(userpostcode);
-
-    }
+        }
 });
 
 
+
+
+
+// Energy center - Address Error
+router.get('/' + version + '/add-heat-network/energycentre/addresserror', function (req, res) {
+    
+    const urlParams = req.query.reason;
+    req.session.data['addresserrorreason'] = urlParams;
+
+    res.render('/' + version + '/add-heat-network/energycentre/addresserror', {
+        data: req.session.data
+    });
+});
 
 
 
@@ -6779,7 +6825,7 @@ router.post('/' + version + '/add-heat-network/buildingsandconsumers/nondomestic
 
             if (customersnondomestic == "Yes"){   
                 if (customersdomestic == "Yes") {
-                    res.redirect('/' + version + '/add-heat-network/buildingsandconsumers/smallmediumbusinesses');
+                    res.redirect('/' + version + '/add-heat-network/buildingsandconsumers/smallenterprises');
                 }
                 else {
                     res.redirect('/' + version + '/add-heat-network/buildingsandconsumers/industrial');
